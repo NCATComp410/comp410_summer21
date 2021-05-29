7841bf111bb43a6ad25ca89ae31fd384ba2e0662
mkc <claesmk@gmail.com>
2021-05-29 07:27:35 -0400

    Added demo

---
+++ demo.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def show_aggie_pride():
+    # https://pandas.pydata.org/docs/user_guide/index.html
+    df = pd.DataFrame(['Aggie Pride', 'Worldwide'])
+
+    print(df)
+
+
+if __name__ == "__main__":
+    show_aggie_pride()
