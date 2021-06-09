973b356a797f1d45eca7e52f02f300732f9de62b
pamichals <59964219+pamichals@users.noreply.github.com>
2021-06-08 20:11:46 -0400

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(df.loc[0, 'Text'], 'Aggie Pride')
         self.assertEqual(df.loc[1, 'Text'], 'Worldwide')
-        self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')
+        self.assertEqual(df.loc[2, 'Text'], 'Aggies Rule')
 
 
 if __name__ == '__main__':
