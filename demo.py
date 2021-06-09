c9c73ea74df25053fdb10b0ad44678b5e06d4a89
KJScott00 <46763253+KJScott00@users.noreply.github.com>
2021-06-09 04:55:45 -0400

    New Slogan Added

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def show_aggie_pride():
     # https://pandas.pydata.org/docs/user_guide/index.html
-    df = pd.DataFrame(['Aggie Pride', 'Worldwide','Hard Work Yields Results'], columns=['Text'])
+    df = pd.DataFrame(['Aggie Pride', 'Worldwide', 'Giving Back Every Day, the Aggie Way'], columns=['Text'])
 
     return df
 
