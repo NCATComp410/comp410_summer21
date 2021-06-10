91b19b67e2ff6e6b0c360e36bc40cf50f13329ee
ahenry890 <ahenry3@aggies.ncat.edu>
2021-06-09 23:00:48 -0400

    Added My slogan

--- test_demo.py
+++ test_demo.py
@@ -10,7 +10,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(df.loc[0, 'Text'], 'Aggie Pride')
         self.assertEqual(df.loc[1, 'Text'], 'Worldwide')
         self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')
-
+        self.assertEqual(df.loc[3, 'Text'], 'Aggies DO!')
 
 if __name__ == '__main__':
     unittest.main()
