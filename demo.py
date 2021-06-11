c0c7fd6fb7ce559b5ed1650bc0c266900068b274
ScottC44 <slcromling@aggies.ncat.edu>
2021-06-10 20:46:44 -0400

    fixed without overwriting

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def show_aggie_pride():
     # https://pandas.pydata.org/docs/user_guide/index.html
-    df = pd.DataFrame(['Work today for what you want to achieve tomorrow', 'Try and be just 1% better everyday','A calm mind is the key to sound thinking'], columns=['Text'])
+    df = pd.DataFrame(['Aggie Pride', 'Worldwide','Hard Work Yields Results','Work today for what you want to achieve tomorrow'], columns=['Text'])
 
     return df
 
--- test_demo.py
+++ test_demo.py
@@ -6,10 +6,11 @@ from demo import show_aggie_pride
 class MyTestCase(unittest.TestCase):
     def test_show_aggie_pride(self):
         df = show_aggie_pride()
-
+        self.assertEqual(df.loc[0, 'Text'], 'Aggie Pride')
+        self.assertEqual(df.loc[1, 'Text'], 'Worldwide')
+        self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')        
         self.assertEqual(df.loc[0, 'Text'], 'Work today for what you want to achieve tomorrow')
-        self.assertEqual(df.loc[1, 'Text'], 'Try and be just 1% better everyday')
-        self.assertEqual(df.loc[2, 'Text'], 'A calm mind is the key to sound thinking')
+        
 
 
 if __name__ == '__main__':
