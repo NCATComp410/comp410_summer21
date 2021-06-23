e3e9f96eb7bc3a66c1a05907ee91111e406ca4ca
ahenry890 <54510575+ahenry890@users.noreply.github.com>
2021-06-22 18:24:19 -0700

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -10,7 +10,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(df.loc[1, 'Text'], 'Worldwide')
         self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')
         self.assertEqual(df.loc[3, 'Text'], 'Work today for what you want to achieve tomorrow')
-        self.assertEqual(df.loc[4, 'Text'], 'Aggies DO!')
+        self.assertEqual(df.loc[4, 'Text'], 'Aggies Do!')
 	self.assertEqual(df.loc[5, 'Text'], 'Giving Back Every Day, the Aggie Way')
 
 
