26177141139bc225b522a8b5fdbb0b5b9c2ada10
ahenry890 <ahenry3@aggies.ncat.edu>
2021-07-13 23:01:24 -0400

    Changes

--- demo.py
+++ demo.py
@@ -1,13 +1,47 @@
 import pandas as pd
+ from log_parse import LogParse
 
 
-def show_aggie_pride():
-    # https://pandas.pydata.org/docs/user_guide/index.html
-    df = pd.DataFrame(['Aggie Pride', 'Worldwide','Hard Work Yields Results',
-                       'Work today for what you want to achieve tomorrow','Aggies Do!',
-                       'Giving Back Every Day, the Aggie Way'], columns=['Text'])
-    return df
+ def show_aggie_pride():
+     # https://pandas.pydata.org/docs/user_guide/index.html
+
+     df = pd.DataFrame(['Aggie Pride', 'Worldwide', 'Hard Work Yields Results',
+                        'Work today for what you want to achieve tomorrow', 'Aggies DO!',
+                        'Giving Back Every Day, the Aggie Way'], columns=['Text'])
+                        'Giving Back Every Day, the Aggie Way', 'Aggies Rule',
+                        'When One Door Closes Another One Opens', 'Aggie Pride',
+                        'Aggies on fire'], columns=['Text'])
+
+     return df
 
 
-if __name__ == "__main__":
-    print(show_aggie_pride())
+ def parse_logs():
+     parser = LogParse()
+     df = parser.parse_syslog_file('syslogs.txt')
+     print('Total syslog found', end=': ')
+     print(len(df))
+
+
+ def asa_parse():
+     # Parse the ASA logfile
+     parser = LogParse()
+     df = parser.parse_asa_logfile('dmz1_logs.txt')
+     print(df.describe())
+
+     # Create a table by hour with the total count of messages seen
+     table = df.groupby([pd.Grouper(key='Date', freq='H')]).agg({'ID': 'count'})
+
+     # Create a simple plot and save as a png
+     fig = table.plot().get_figure()
+     fig.savefig('plot.png')
+
+
+ if __name__ == "__main__":
+     print(show_aggie_pride())
+
+     print('\nCalling parse_logs()')
+     parse_logs()
+
+     # Uncomment when we start sprint-4
+     # print('\nCalling asa_parse()')
+     # asa_parse()
--- test_demo.py
+++ test_demo.py
@@ -1,18 +1,25 @@
 import unittest
 from demo import show_aggie_pride
-
-
 # https://docs.python.org/3/library/unittest.html
-class MyTestCase(unittest.TestCase):
-    def test_show_aggie_pride(self):
-        df = show_aggie_pride()
-        self.assertEqual(df.loc[0, 'Text'], 'Aggie Pride')
-        self.assertEqual(df.loc[1, 'Text'], 'Worldwide')
-        self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')
-        self.assertEqual(df.loc[3, 'Text'], 'Work today for what you want to achieve tomorrow')
-        self.assertEqual(df.loc[4, 'Text'], 'Aggies Do!')
-	self.assertEqual(df.loc[5, 'Text'], 'Giving Back Every Day, the Aggie Way')
+ class MyTestCase(unittest.TestCase):
+     def test_show_aggie_pride(self):
+         df = show_aggie_pride()
+
+         self.assertEqual(df.loc[0, 'Text'], 'Aggie Pride')
+         self.assertEqual(df.loc[1, 'Text'], 'Worldwide')
+         self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')
+         self.assertEqual(df.loc[2, 'Text'], 'Hard Work Yields Results')
+         self.assertEqual(df.loc[3, 'Text'], 'Work today for what you want to achieve tomorrow')
+         self.assertEqual(df.loc[4, 'Text'], 'Aggies DO!')
+         self.assertEqual(df.loc[5, 'Text'], 'Giving Back Every Day, the Aggie Way')
+         self.assertEqual(df.loc[6, 'Text'], 'Aggies Rule')
+         self.assertEqual(df.loc[7, 'Text'], 'When One Door Closes Another One Opens')
+         self.assertEqual(df.loc[8, 'Text'], 'Aggie Pride')
+         self.assertEqual(df.loc[9, 'Text'], 'Aggies on fire')
 
 
-if __name__ == '__main__':
+
+
+ if __name__ == '__main__':
     unittest.main()
+
