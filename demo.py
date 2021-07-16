1c3137c20f2c839d51993f7c67f3962841e6e5f4
ahenry890 <ahenry3@aggies.ncat.edu>
2021-07-15 23:13:06 -0400

    Final Changes

--- demo.py
+++ demo.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 def show_aggie_pride():
     # https://pandas.pydata.org/docs/user_guide/index.html
-    df = pd.DataFrame(['Aggie Pride', 'Worldwide','Hard Work Yields Results', 'Aggies DO!'], columns=['Text'])
+    df = pd.DataFrame(['Aggie Pride', 'Worldwide','Hard Work Yields Results',
+                       'Work today for what you want to achieve tomorrow', 'Aggies DO!',
+                       'Giving Back Every Day, the Aggie Way'], columns=['Text'])
 
     return df
 
